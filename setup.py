1b6d92ba1f5eee041cd7f8af22c9c64f7cff4cb1
Your Name <surendraputta47>
2023-08-26 16:22:57 +0000

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+
+setup(
+ name='mlproject',
+ version='0.0.1',
+ author='surendra',
+ author_email='surendraputta47@gmail.com',
+ packages=find_packages(),
+ install_requires=['pands','numpy','seaborn'],
+
+)
+
